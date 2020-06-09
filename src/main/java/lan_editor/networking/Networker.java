2d2923976cb32812595e99c0727eddb8e3a9962e
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-06-09 20:02:35 +0200

    Networker działa w trybie klienta

--- src/main/java/lan_editor/networking/Networker.java
+++ src/main/java/lan_editor/networking/Networker.java
@@ -1,29 +1,56 @@
 package lan_editor.networking;
 
+import com.sun.tools.javac.Main;
 import lan_editor.gui.MainGuiController;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.channels.SocketChannel;
+import java.util.Optional;
 
 /**
  * Klasa zajmująca się komunikacją sieciową z serwerem i innymi klientami
  */
 
 public class Networker implements Runnable {
-    Dispatcher dispatcher;
+    private Dispatcher dispatcher;
+    private boolean iAmServer;
+
+    private int port;
+    private String address;
 
     MainGuiController gui;
-    public Networker(MainGuiController gui) {
+    public Networker(MainGuiController gui, boolean isServer, String address, int port) {
+        if (address == null && !isServer)
+            throw new IllegalArgumentException("adres nie moze byc null dla klienta");
+        this.address = address;
+        this.port = port;
+        this.iAmServer = isServer;
         this.gui = gui;
         this.dispatcher = new Dispatcher();
+    }
+
+    public static Networker makeServer(MainGuiController gui, int port) {
+        return new Networker(gui, true, null, port);
+    }
+
+    public static Networker makeClient(MainGuiController gui, String address, int port) {
+        return new Networker(gui, false, address, port);
     }
 
     ServerSocket sock;
     @Override
     public void run() {
+        if (iAmServer)
+             server();
+        else client();
+    }
+
+    private void server() {
         try {
-            sock = new ServerSocket(8080);
+            sock = new ServerSocket(port);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -37,5 +64,22 @@ public class Networker implements Runnable {
                 thread.start();
             } catch (IOException e) {e.printStackTrace();}
         }
+    }
+
+    private void client() {
+        dispatcher = new Dispatcher();
+        Socket sock;
+        try {
+            sock = new Socket();
+            sock.connect(new InetSocketAddress(address, port));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+            return; // trzeba, bo inaczej ostrzega że zmienna sock może być niezainicjalizowana
+        }
+        dispatcher.addSocket(sock);
+        var handlerThread = new Thread(new SocketHandler(gui, dispatcher, sock));
+        handlerThread.setDaemon(true);
+        handlerThread.start();
     }
 }
