937233fdf65b35cdf693c2ccd8ac9d8f97e3f31b
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-06-13 17:47:54 +0200

    zmieniony test modułu sieciowego

--- src/main/java/tests/NetworkingTests.java
+++ src/main/java/tests/NetworkingTests.java
@@ -18,10 +18,11 @@ public class NetworkingTests {
 
         var list = new ArrayList<Networker<String>>();
         for (int i = 0; i < 5; i++) {
+            var index = i + 1;
             var klient = Networker.<String>makeClient(
                     "127.0.0.1",
                     8080,
-                    str -> System.out.println("klient: " + str)
+                    str -> System.out.println("klient " + index +": " + str)
             );
             list.add(klient);
             var thread = new Thread(klient);
@@ -30,7 +31,19 @@ public class NetworkingTests {
         }
 
         server.send("Hello");
+        server.send("Hello2");
+        server.send("Hello3");
         list.get(1).send("od klienta");
+
+        Thread.sleep(1000 * 2);
+
+        var newClient = Networker.makeClient(
+                "localhost", 8080,
+                str -> System.out.println("nowy klient: " + str)
+        );
+        var newThread = new Thread(newClient);
+        newThread.setDaemon(true);
+        newThread.start();
 
         Thread.sleep(1000 * 2);
 
@@ -43,23 +56,52 @@ public class NetworkingTests {
 //        added sock: localhost
 //        sending "Hello" to localhost
 //        sending "od klienta" to localhost
+//        sending "Hello2" to localhost
+//        sending "Hello3" to localhost
 //        accepted: localhost
 //        added sock: localhost
 //        sending "Hello" to localhost
+//        sending "Hello2" to localhost
+//        sending "Hello3" to localhost
 //        accepted: localhost
 //        added sock: localhost
 //        sending "Hello" to localhost
+//        sending "Hello2" to localhost
+//        sending "Hello3" to localhost
 //        accepted: localhost
 //        added sock: localhost
 //        sending "Hello" to localhost
+//        sending "Hello2" to localhost
+//        sending "Hello3" to localhost
 //        accepted: localhost
 //        added sock: localhost
 //        sending "Hello" to localhost
-//        klient: Hello
-//        klient: Hello
-//        klient: Hello
-//        klient: Hello
+//        sending "Hello2" to localhost
+//        sending "Hello3" to localhost
 //        serwer: od klienta
-//        klient: Hello
+//        klient 2: Hello
+//        klient 3: Hello
+//        klient 2: Hello2
+//        klient 4: Hello
+//        klient 1: Hello
+//        klient 4: Hello2
+//        klient 4: Hello3
+//        klient 5: Hello
+//        klient 1: Hello2
+//        klient 5: Hello2
+//        klient 5: Hello3
+//        klient 2: Hello3
+//        klient 3: Hello2
+//        klient 1: Hello3
+//        klient 3: Hello3
+//        added sock: localhost
+//        accepted: localhost
+//        added sock: localhost
+//        sending "Hello" to localhost
+//        sending "Hello2" to localhost
+//        sending "Hello3" to localhost
+//        nowy klient: Hello
+//        nowy klient: Hello2
+//        nowy klient: Hello3
     }
 }
